415882284ca621e3ef69097d9f15ad0ccebdb857
Sadanand09 <sadanandgurude9@gmail.com>
2024-11-14 23:44:47 +0530

    ref: commiting changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./routes/home/home.component"
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 
-const Navigation = () => {
-  return(
-    <div>
-      <h1>Hii I'm the Navigation page.</h1>
-    </div>
-  )
-}
+
 
 const Shop = () => {
-  return(
+  return (
     <div>
       <h1>Hii I'm the Shop page.</h1>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
-
     <Routes>
-      <Route path="/home" element= {<Home />}/>
-        <Route path="/shop" element={<Shop />}  />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
-  
   );
-} 
+};
 
 export default App;
