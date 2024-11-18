54b217acdcb74180be4f96753c717b2e24a69a91
Sadanand09 <sadanandgurude9@gmail.com>
2024-11-18 22:50:18 +0530

    ref: Commiting changes

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +26,9 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup( auth, provider );
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+
+}
