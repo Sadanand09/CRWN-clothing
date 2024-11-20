cfcc5d4fea4cb827dca9a978d92f35ad46810db5
Sadanand09 <sadanandgurude9@gmail.com>
2024-11-20 14:36:09 +0530

    ref: finished the creating user document

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,10 +3,8 @@ import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/f
 const  SignIn =  () => {
 
     const logGoogleUser = async () => {
-
-        const response = await signInWithGooglePopup();
-        createUserDocumentFromAuth(response);
-
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return(
