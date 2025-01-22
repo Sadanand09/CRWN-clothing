d23cd9a3a432aabbcf29d6ece2582f1625dc4ba1
Sadanand09 <sadanandgurude9@gmail.com>
2025-01-22 19:16:33 +0530

    ref: category component updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
 
@@ -15,13 +14,11 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState, Fragment } from "react";
+import { useEffect, useState, Fragment } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 import ProductCard from "../../component/product-card/product-card.component";
 
@@ -9,7 +10,7 @@ import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState([category]);
 
   useEffect(() => {
